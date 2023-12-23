c8676d4d26d0de535bfbec170392d0b7d35fe5ee
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-23 12:00:17 +0100

    listing software

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import softwares from './data/softwares.json';
+
 function App() {
 
-  return (
-    <>
-    <h1>Sort Test</h1>
-    <p>testing</p>
-    </>
-  )
+	return (
+		<>
+		<h1>Sort Test</h1>
+			<p>There are {softwares.length} softwares.</p>
+			{softwares.map((software, index) => {
+				return (
+					<div key={index}>{software.name}</div>
+				)
+			})}
+		</>
+	)
 }
 
 export default App
