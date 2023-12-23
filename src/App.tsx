b420cfd57b676729b697522a7db95343744a202f
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-23 12:19:01 +0100

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import softwares from './data/softwares.json';
+import { useState } from 'react';
+import rawFlashcards from './data/flashcards.json';
 
-softwares.sort((a, b) => a.rank > b.rank ? 1 : -1);
+rawFlashcards.sort((a, b) => a.rank > b.rank ? 1 : -1);
+
+interface IFlashcard {
+	name: string,
+	rank: number
+}
 
 function App() {
+	const [flashcards, setFlashcards] = useState<IFlashcard[]>(rawFlashcards);
+
+	const handleRankChange = (flashcard:IFlashcard, e:React.ChangeEvent<HTMLInputElement>) => {
+		const value = Number(e.target.value);
+		flashcard.rank = value;
+flashcards.sort((a, b) => a.rank > b.rank ? 1 : -1);
+		const _flashcards = structuredClone(flashcards);
+		setFlashcards(_flashcards);
+	}
 
 	return (
 		<>
 		<h1>Sort Test</h1>
-			<p>There are {softwares.length} softwares.</p>
-			{softwares.map((software, index) => {
+			<p>There are {flashcards.length} flashcards.</p>
+			{flashcards.map((flashcard, index) => {
 				return (
-					<div key={index}>{software.name} - rank: {software.rank}</div>
+					<div key={index}>{flashcard.name} - rank: {flashcard.rank} - <input value={flashcard.rank} onChange={(e) => handleRankChange(flashcard, e)}/></div>
 				)
 			})}
 		</>
